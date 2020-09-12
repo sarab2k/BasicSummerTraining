280ea3c1222b7552fe45716883eda7ff683d8e93
Sarabpreet Singh <71119090+sarab2k@users.noreply.github.com>
2020-09-12 09:54:00 +0530

    Update Assignemnt c3 W4 Part2 .py

--- Assignemnt c3 W4 Part2 .py
+++ Assignemnt c3 W4 Part2 .py
@@ -6,7 +6,6 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
-#RUNNING THE FIRST html
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -16,11 +15,11 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('a')
 for i in range(count):
     lst.append(tags[pos].get('href', None))
-#REPEATED ITERATIONS OF ABOVE code3
 
-#while count != 0 :
-#    html = urllib.request.urlopen(lst[pos], context=ctx).read()
-#    soup = BeautifulSoup(html, 'html.parser')
-#    tags = soup('a')
-#    for tag in tags:
-#        lst.append(tag.get('href', None))
+
+while count != 0 :
+    html = urllib.request.urlopen(lst[pos], context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    for tag in tags:
+        lst.append(tag.get('href', None))
